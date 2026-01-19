946cb03c784de56512871b02ea547ee0da0c1b08
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-19 22:15:55 +0900

    feat: InventoryLogモデルにバリデーションを追加

--- app/models/inventory_log.rb
+++ app/models/inventory_log.rb
@@ -1,3 +1,15 @@
 class InventoryLog < ApplicationRecord
   belongs_to :item
+
+  enum reason: { purchase: 0, consume: 1, dispose: 2 }
+
+  # バリデーション
+  # nullチェック
+  validates :item_id, presence: true
+  validates :change_amount, presence: true
+  validates :reason, presence: true
+  # change_amountは0より大きい数値か？
+  validates :change_amount, numericality: { other_than: 0 }
+  # reasonはenumの値だけか？
+  validates :reason, inclusion: { in: reasons.keys }
 end
