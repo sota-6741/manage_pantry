f4387d82d0594ac0c700ebd880643c5ad0eb6ab8
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-20 14:14:50 +0900

    feat: createメソッドをItemsControllerに実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,16 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      # 成功した場合はUX的に新規作成画面にリダイレクトした方が使いやすいかも
+      redirect_to new_item_path, notice: "アイテムを追加しました"
+    else
+      # 失敗したらパラメータを残して新規作成画面に戻す
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
