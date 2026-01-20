acb7e8c7acb06135d34f7480a7a4553b04a7d1ee
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-20 14:28:11 +0900

    feat: updateメソッドをItemsControllerに追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,4 +22,13 @@ class ItemsController < ApplicationController
     redirect_to items_path, notice: "アイテムを削除しました"
   end
 
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to items_path, notice: "アイテムを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
