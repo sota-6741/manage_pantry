f4ef2160bff8bc89dcfb0f049314fe66a49f65fe
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-22 16:53:59 +0900

    feat: InventoryLogsControllerにindexメソッドを追加

--- app/controllers/inventory_logs_controller.rb
+++ app/controllers/inventory_logs_controller.rb
@@ -1,2 +1,6 @@
 class InventoryLogsController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @inventory_logs = @item.inventory_logs.order(created_at: :desc)
+  end
 end
