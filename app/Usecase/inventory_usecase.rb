59f019268037cdce40c67e136c6abbd8e1d6be4b
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-23 20:50:19 +0900

    refactor: InventoryUsecaseクラスのインスタンス変数を修正し、selfの使用を削除

--- app/Usecase/inventory_usecase.rb
+++ app/Usecase/inventory_usecase.rb
@@ -1,41 +1,42 @@
 class InventoryUsecase
   include ReasonDefinition
+  attr_reader :item, :inventory_log
 
-  def initialize(item, inventory_log)
-    self.item = item
-    self.inventory_log = inventory_log
+  def initialize(item_model, inventory_log_model)
+    @item = item_model
+    @inventory_log = inventory_log_model
   end
 
   # アイテム管理
   def create_item(params)
     ActiveRecord::Base.transaction do
-      item = self.item.new(params)
-      inventory_log = self.inventory_log.build(
+      item = @item.new(params)
+      item.save!
+      inventory_log = @inventory_log.build(
         item: item,
         amount: item.quantity,
         reason: :purchase
       )
-      item.save!
       inventory_log.save!
       item
     end
   end
 
   def update_item(item_id, params)
-    item = self.item.find(item_id)
+    item = @item.find(item_id)
     item.update!(params)
     item
   end
 
   def delete_item(item_id)
-    item = self.item.find(item_id)
+    item = @item.find(item_id)
     item.destroy!
   end
 
   # 在庫操作
   def change_stock(item_id:, amount:, reason:)
     raise ArgumentError, "理由が不適切です: #{reason}" unless reason.to_sym.in?(REASONS)
-    item = self.item.find(item_id)
+    item = @item.find(item_id)
 
     delta =
     case reason.to_sym
@@ -43,10 +44,10 @@ class InventoryUsecase
     when :consume, :dispose then -amount
     end
 
-    self.item.transaction do
+    @item.transaction do
       raise StandardError, "在庫が不足しています" unless item.can_change_quantity?(delta)
       item.update!(quantity: item.quantity + delta)
-      self.inventory_log.create!(
+      @inventory_log.create!(
         item: item,
         change_amount: delta,
         reason: reason
