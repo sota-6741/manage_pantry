ae95013eb5b0f162647da34beafaa5596655e628
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-23 19:59:10 +0900

    refactor: InventoryUsecaseクラスの初期化メソッドを修正し、アイテム管理メソッドを整理

--- app/Usecase/inventory_usecase.rb
+++ app/Usecase/inventory_usecase.rb
@@ -1,17 +1,19 @@
 class InventoryUsecase
-  def initialize(item_model: Item, inventory_log_model: InventoryLog)
-    @item = item_model
-    @inventory_log = inventory_log_model
+  include ReasonDefinition
+
+  def initialize(item, inventory_log)
+    self.item = item
+    self.inventory_log = inventory_log
   end
 
-  # アイテム管理系
+  # アイテム管理
   def create_item(params)
-    transition do
-      item = @item.new(params)
-      inventory_log = @inventory_log.build(
+    ActiveRecord::Base.transaction do
+      item = self.item.new(params)
+      inventory_log = self.inventory_log.build(
         item: item,
         amount: item.quantity,
-        reason: purchase
+        reason: :purchase
       )
       item.save!
       inventory_log.save!
@@ -19,21 +21,38 @@ class InventoryUsecase
     end
   end
 
-  def edit_item(item_id, params)
-    item = @item.find(item_id)
+  def update_item(item_id, params)
+    item = self.item.find(item_id)
     item.update!(params)
     item
   end
 
   def delete_item(item_id)
-    item = @item.find(item_id)
-    item.destory!
+    item = self.item.find(item_id)
+    item.destroy!
   end
 
-  # 在庫操作形
-  def purchase(item_id, amount)
-    change_stock(item_id, amount)
-  end
+  # 在庫操作
+  def change_stock(item_id:, amount:, reason:)
+    raise ArgumentError, "理由が不適切です: #{reason}" unless reason.to_sym.in?(REASONS)
+    item = self.item.find(item_id)
 
-  def dispose
+    delta =
+    case reason.to_sym
+    when :purchase then amount
+    when :consume, :dispose then -amount
+    end
+
+    self.item.transaction do
+      raise StandardError, "在庫が不足しています" unless item.can_change_quantity?(delta)
+      item.update!(quantity: item.quantity + delta)
+      self.inventory_log.create!(
+        item: item,
+        change_amount: delta,
+        reason: reason
+      )
+    end
+
+    item
+  end
 end
