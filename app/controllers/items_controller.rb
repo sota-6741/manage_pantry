868dd3e2e9919432968542d37f86662af032a28a
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-24 22:05:56 +0900

    feat: ItemsControllerをリファクタリングし、ユースケースを使用してアイテムの作成、更新、削除機能を改善

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,70 +4,56 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = InventoryService.create_item(item_params)
-
-    # サービスが返したオブジェクトがDBに保存済み（＝成功）かチェック
-    if @item.persisted?
-      redirect_to new_item_path, notice: "アイテムを追加しました"
-    else
-      # 失敗時はエラーの入った@itemを使い、新規作成画面を再描画
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-
-    @item.destroy
-    redirect_to items_path, notice: "アイテムを削除しました"
+    usecase = Inventory::CreateItemUsecase.new
+    @item = usecase.call(item_params)
+    redirect_to new_item_path, notice: "アイテムを追加しました"
+  rescue ActiveRecord::RecordInvalid => e
+    # 保存失敗時は例外から item を取り出してフォームに渡す
+    @item = e.record
+    render :new, status: :unprocessable_entity
   end
 
   def update
-    @item = Item.find(params[:id])
-
-    if @item.update(item_params)
-      redirect_to items_path, notice: "アイテムを更新しました"
-    else
-      render :edit, status: :unprocessable_entity
-    end
+    usecase = UpdateItemUsecase.new
+    @item = usecase.call(params[:id], item_update_params)
+    redirect_to items_path, notice: "アイテムを更新しました"
+  rescue ActiveRecord::RecordInvalid => e
+    @item = e.record
+    render :edit, status: :unprocessable_entity
   end
 
-  def consume
-    @item = Item.find(params[:id])
-    amount = params[:amount].to_i
-
-    if InventoryService.change_quantity(item: @item, amount: amount, reason: :consume)
-      redirect_to items_path, notice: "在庫を消費しました"
-    else
-      redirect_to items_path, alert: @item.errors.full_messages.to_sentence
-    end
+  def destroy
+    usecase = DeleteItemUsecase.new
+    @item = usecase.call(params[:id])
+    redirect_to items_path, notice: "アイテムを削除しました"
   end
 
-  def dispose
+
+  def change_stock
+    usecase = Inventory::ChangeStockUsecase.new
+    @item = usecase.call(
+      item_id: params[:id],
+      amount: change_stock_params[:amount].to_i,
+      reason_key: change_stock_params[:reason]
+    )
+    redirect_to items_path, notice: "アイテムの在庫数を変更しました"
+  rescue StandardError => e
     @item = Item.find(params[:id])
-    amount = params[:amount].to_i
-
-    if InventoryService.change_quantity(item: @item, amount: amount, reason: :dispose)
-      redirect_to items_path, notice: "在庫を破棄しました"
-    else
-      redirect_to items_path, alert: @item.errors.full_messages.to_sentence
-    end
-  end
-
-  def purchase
-    @item = Item.find(params[:id])
-    amount = params[:amount].to_i
-
-    if InventoryService.change_quantity(item: @item, amount: amount, reason: :purchase)
-      redirect_to items_path, notice: "在庫を追加しました"
-    else
-      redirect_to items_path, alert: @item.errors.full_messages.to_sentence
-    end
+    flash.now[:alert] = e.message
+    render :change_stock, status: :unprocessable_entity
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :quantity, :expiration_date)
+    params.require(:item).permit(:name, :quantity, :expiration_date, :category_id)
+  end
+
+  def item_update_params
+    params.require(:item).permit(:name, :expiration_date)
+  end
+
+  def change_stock_params
+    params.require(:item).permit(:amount, :reason)
   end
 end
