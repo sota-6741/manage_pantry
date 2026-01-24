b7c62fd44284b43c82016ff388f2c9d19e6187c5
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-24 22:08:05 +0900

    feat: ItemsControllerTestにアイテム作成と在庫変更のテストを追加

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class ItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @item = items(:one)
+    @category = categories(:one)
+  end
+
+  test "アイテムを作成できる" do
+    assert_difference(["Item.count", "InventoryLog.count"], 1) do
+      post items_url, params: { item: { name: "New Item", quantity: 5, expiration_date: "2026-12-31", category_id: @category.id } }
+    end
+    assert_redirected_to new_item_path
+    assert_equal "purchase", InventoryLog.last.reason
+  end
+
+  test "在庫を変更できる" do
+    assert_difference("InventoryLog.count", 1) do
+      patch change_stock_item_url(@item), params: { item: { amount: 3, reason: "consume" } }
+    end
+    assert_redirected_to items_path
+    
+    @item.reload
+    assert_equal "consume", InventoryLog.last.reason
+  end
 end
