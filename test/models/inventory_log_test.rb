c87f76285a9ded74b3f51b79d7955bc8ab817910
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-24 22:22:14 +0900

    feat: InventoryLogのテストを追加し、バリデーションとログ保存の検証を行う

--- test/models/inventory_log_test.rb
+++ test/models/inventory_log_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class InventoryLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @item = items(:one)
+  end
+
+  test "バリデーション: 必須項目が欠けている場合は無効" do
+    log = InventoryLog.new
+    assert_not log.valid?
+    assert_includes log.errors[:item_id], "can't be blank" # items(:one) が存在するためFK制約以前にpresenceバリデーション
+    assert_includes log.errors[:change_amount], "can't be blank"
+    assert_includes log.errors[:reason], "can't be blank"
+  end
+
+  test "inventory_reason: reasonカラムからInventoryReasonオブジェクトを取得できる" do
+    log = InventoryLog.new(reason: "purchase")
+    assert_instance_of InventoryReason, log.inventory_reason
+    assert_equal :purchase, log.inventory_reason.reason_key
+  end
+
+  test "record: ログを保存できる" do
+    assert_difference("InventoryLog.count", 1) do
+      InventoryLog.record(item: @item, delta: -5, reason_key: :consume)
+    end
+    
+    log = InventoryLog.last
+    assert_equal @item, log.item
+    assert_equal(-5, log.change_amount)
+    assert_equal "consume", log.reason
+    assert_equal :consume, log.inventory_reason.reason_key
+  end
 end
