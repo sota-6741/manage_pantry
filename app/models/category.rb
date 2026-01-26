7e5e25c35f9c922776e6717ecdf3066eb7b8df40
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-26 14:11:06 +0900

    feat: CategoryモデルにDB操作のメソッド実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,29 @@
 class Category < ApplicationRecord
+  has_many :items
+
+  validates :name, presence: true, uniqueness: true
+
+  class << self
+    def register(params)
+      create!(params)
+    end
+
+    def fetch(id)
+      find(id)
+    end
+
+    def destroy(id)
+      category = fetch(id)
+      category.destroy!
+    end
+
+    def update(id, params)
+      item = fetch(id)
+      item.update!(params)
+    end
+
+    def all_fetch
+      all
+    end
+  end
 end
