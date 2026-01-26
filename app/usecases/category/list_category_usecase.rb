0f56112a845a66d5bd6a458b7d19ed79c3aa1ac5
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-26 14:11:35 +0900

    feat: ListCategoryUsecaseを新規作成し、カテゴリ一覧取得機能を実装

---
+++ app/usecases/category/list_category_usecase.rb
@@ -0,0 +1,12 @@
+module Category
+  class ListCategoryUsecase
+    def initialize(category_model: Category)
+      @category_model = category_model
+    end
+
+    def call
+      categories = @category_model.all_fetch
+      categories
+    end
+  end
+end
