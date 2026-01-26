2fde0a0d425a40a3bf1f90eb62ecaccd31ba905d
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-01-26 12:31:40 +0900

    refactor: ListItemsUsecaseでN+1問題を防ぐためにlistItemsUsecaseで関連wo EagerLoadする

--- app/usecases/inventory/list_items_usecase.rb
+++ app/usecases/inventory/list_items_usecase.rb
@@ -2,8 +2,8 @@
 module Inventory
   class ListItemsUsecase
     def call
-      near_expiration_items = Item.near_expiration_items
-      other_items = Item.where.not(id: near_expiration_items.select(:id))
+      near_expiration_items = Item.near_expiration_items.includes(:category, :inventory_logs)
+      other_items = Item.where.not(id: near_expiration_items.select(:id)).includes(:category, :inventory_logs)
       near_expiration_items + other_items
     end
   end
