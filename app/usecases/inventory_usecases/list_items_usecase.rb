5cde79b56077616f89ac1e83046d1f30f28095cc
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-02-15 16:48:17 +0900

    refactor: ListItemsUsecaseのcallメソッドを修正し、ユーザーからアイテムを取得するように変更

--- app/usecases/inventory_usecases/list_items_usecase.rb
+++ app/usecases/inventory_usecases/list_items_usecase.rb
@@ -5,12 +5,9 @@ module InventoryUsecases
       @category_model = category_model
     end
 
-    def call(category_id: nil)
-      base_scope = if category_id.present?
-        @item_model.where(category_id: category_id)
-      else
-        @item_model.all
-      end
+    def call(user:, category_id: nil)
+      base_scope = user.items
+      base_scope = base_scope.where(category_id: category_id) if category_id.present?
 
       base_scope.includes(:category, :inventory_logs).ordered_by_urgency
     end
