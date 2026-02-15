1432cfa54d7e19e6178d18aecf26ee3f554bd952
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-02-15 16:47:15 +0900

    refactor: Categoryモデルのnameバリデーションにuser_idスコープを追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
+  belongs_to :user, optional: true
   has_many :items
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 end
