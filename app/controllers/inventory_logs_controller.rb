b78716f599d5c2698cdccacc3651f3a7ba58b96d
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-02-15 16:45:57 +0900

    refactor: InventoryLogsControllerでのユーザー情報を考慮した在庫ログ取得処理の修正

--- app/controllers/inventory_logs_controller.rb
+++ app/controllers/inventory_logs_controller.rb
@@ -1,7 +1,6 @@
 class InventoryLogsController < ApplicationController
   def index
-    usecase = ShowInventoryLogUsecase.new
-    @item, @inventory_logs = usecase.call(params[:item_id])
+    @item, @inventory_logs = InventoryUsecases::ShowInventoryLogUsecase.new(user: current_user).call(params[:item_id])
   rescue ActiveRecord::RecordNotFound
     redirect_to items_path, alert: "アイテムが見つかりません"
   end
