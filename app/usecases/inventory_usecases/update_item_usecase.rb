0211f455a112397013b9771bd2f107be2a6a4c70
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-02-15 16:48:25 +0900

    refactor: UpdateItemUsecaseの初期化メソッドを修正し、ユーザーからアイテムを取得するように変更

--- app/usecases/inventory_usecases/update_item_usecase.rb
+++ app/usecases/inventory_usecases/update_item_usecase.rb
@@ -1,11 +1,11 @@
 module InventoryUsecases
   class UpdateItemUsecase
-    def initialize(item_model: Item)
-      @item_model = item_model
+    def initialize(user:)
+      @user = user
     end
 
     def call(item_id, params)
-      item = @item_model.find(item_id)
+      item = @user.items.find(item_id)
       item.update!(params)
       item
     end
