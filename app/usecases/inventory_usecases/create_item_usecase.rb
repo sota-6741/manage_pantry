6e7b320303c83c1a546ac4ff43a3b0bbd092ab6c
sota-6741 <32445070@fukuchiyama.ac.jp>
2026-02-15 16:48:05 +0900

    refactor: CreateItemUsecaseの初期化メソッドを修正し、ユーザーからアイテムを作成するように変更

--- app/usecases/inventory_usecases/create_item_usecase.rb
+++ app/usecases/inventory_usecases/create_item_usecase.rb
@@ -1,13 +1,13 @@
 module InventoryUsecases
   class CreateItemUsecase
-    def initialize(item_model: Item, inventory_log_model: InventoryLog)
-      @item_model = item_model
+    def initialize(user:, inventory_log_model: InventoryLog)
+      @user = user
       @inventory_log_model = inventory_log_model
     end
 
     def call(params)
       ActiveRecord::Base.transaction do
-        item = @item_model.create!(params)
+        item = @user.items.create!(params)
         @inventory_log_model.record(item: item, delta: item.quantity, reason_key: :purchase)
         item
       end
